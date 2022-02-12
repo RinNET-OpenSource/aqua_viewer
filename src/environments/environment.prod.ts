38c495613c47ab1d1a6aa97383d27987475d76ca
Dom Eori <4j6dq2zi8@relay.firefox.com>
2022-02-12 18:57:40 +0900

    Strip old static file server URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  assetsHost: '//static.samnyan.icu/'
+  assetsHost: ''
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  assetsHost: '//static.samnyan.icu/'
+  assetsHost: ''
 };
 
 /*
