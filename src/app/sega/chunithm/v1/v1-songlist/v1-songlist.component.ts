69282234a26c7f01374beb4c3a24eb2e9e80aefd
Fleming Karlzett <schdsrlin@gmail.com>
2022-09-06 17:32:26 +0000

    Chuni/Chusan Music Data Fix

--- src/app/sega/chunithm/v1/v1-songlist/v1-songlist.component.ts
+++ src/app/sega/chunithm/v1/v1-songlist/v1-songlist.component.ts
@@ -3,6 +3,9 @@ import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import {ChuniMusic} from '../model/ChuniMusic';
 import {NgxIndexedDBService} from 'ngx-indexed-db';
+import {ApiService} from '../../../../api.service';
+import {AuthenticationService} from '../../../../auth/authentication.service';
+import {MessageService} from '../../../../message.service';
 
 @Component({
   selector: 'app-v1-songlist',
@@ -17,10 +20,33 @@ export class V1SonglistComponent implements OnInit {
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
-  constructor(private dbService: NgxIndexedDBService) {
+  constructor(
+    private dbService: NgxIndexedDBService,
+    private api: ApiService,
+    private auth: AuthenticationService,
+    private messageService: MessageService
+  ) {
+
   }
 
+
   ngOnInit() {
+    // This is a temporary solve method
+    // Reload the database when user access the page
+    this.api.get('api/game/chuni/v1/data/music').subscribe(
+      data => {
+        this.songList = data;
+        this.dataSource.data = this.songList;
+        data.forEach(x => {
+          this.dbService.add<ChuniMusic>('chuniMusic', x).subscribe(
+            () => '', error => {
+              console.error(error);
+            }
+          );
+        });
+      },
+      error => this.messageService.notice(error)
+    );
     this.dbService.getAll<ChuniMusic>('chuniMusic').subscribe(
       x => {
         this.songList = x;
--- src/app/sega/chunithm/v2/v2-songlist/v2-songlist.component.ts
+++ src/app/sega/chunithm/v2/v2-songlist/v2-songlist.component.ts
@@ -3,6 +3,9 @@ import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import {ChusanMusic} from '../model/ChusanMusic';
 import {NgxIndexedDBService} from 'ngx-indexed-db';
+import {ApiService} from '../../../../api.service';
+import {MessageService} from '../../../../message.service';
+
 
 @Component({
   selector: 'app-v2-songlist',
@@ -17,10 +20,29 @@ export class V2SonglistComponent implements OnInit {
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
-  constructor(private dbService: NgxIndexedDBService) {
+  constructor(
+    private dbService: NgxIndexedDBService,
+    private api: ApiService,
+    private messageService: MessageService
+  ) {
   }
 
   ngOnInit() {
+    // Same as v1-songlist.component.ts:36
+    this.api.get('api/game/chuni/v2/data/music').subscribe(
+      data => {
+        this.songList = data;
+        this.dataSource.data = this.songList;
+        data.forEach(x => {
+          this.dbService.add<ChusanMusic>('chusanMusic', x).subscribe(
+            () => '', error => {
+              console.error(error);
+            }
+          );
+        });
+      },
+      error => this.messageService.notice(error)
+    );
     this.dbService.getAll<ChusanMusic>('chusanMusic').subscribe(
       x => {
         this.songList = x;
