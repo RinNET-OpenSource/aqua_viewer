8f140617b45ff5d3c8cfac59b07605ee39d42c33
三黑 <w953934508@outlook.com>
2023-12-04 21:58:38 +0800

    [ongeki] Update song list style

--- src/app/sega/ongeki/ongeki-song-list/ongeki-song-list.component.ts
+++ src/app/sega/ongeki/ongeki-song-list/ongeki-song-list.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgxIndexedDBService} from 'ngx-indexed-db';
 import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
 import {OngekiMusic} from '../model/OngekiMusic';
 import {ApiService} from '../../../api.service';
 import {MessageService} from '../../../message.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-ongeki-song-list',
@@ -12,46 +12,53 @@ import {MessageService} from '../../../message.service';
   styleUrls: ['./ongeki-song-list.component.css']
 })
 export class OngekiSongListComponent implements OnInit {
-
-  dataSource = new MatTableDataSource();
   songList: OngekiMusic[] = [];
+  filteredSongList: OngekiMusic[];
   displayedColumns: string[] = ['id', 'name', 'artistName'];
+  currentPage = 1;
+  totalElements = 0;
+  searchTerm = '';
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor(
     private dbService: NgxIndexedDBService,
-    private api: ApiService,
-    private messageService: MessageService
+    public route: ActivatedRoute,
+    public router: Router
   ) {
   }
 
   ngOnInit() {
-    this.api.get('api/game/ongeki/data/musicList').subscribe(
-      data => {
-        this.songList = data;
-        this.dataSource.data = this.songList;
-        data.forEach(x => {
-          this.dbService.add<OngekiMusic>('ongekiMusic', x).subscribe(
-            () => '', error => {
-              console.error(error);
-            }
-          );
-        });
-      },
-      error => this.messageService.notice(error)
-    );
     this.dbService.getAll<OngekiMusic>('ongekiMusic').subscribe(
       x => {
         this.songList = x;
-        this.dataSource.data = this.songList;
+        this.filteredSongList = [...x];
       }
     );
-    this.dataSource.paginator = this.paginator;
+    this.route.queryParams.subscribe((data) => {
+      if (data.page) {
+        this.currentPage = data.page;
+      }
+    });
   }
 
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  pageChanged(page: number) {
+    this.router.navigate(['ongeki/song'], {queryParams: {page}});
+  }
+  filterSongs() {
+    if (this.searchTerm) {
+      this.filteredSongList = this.songList.filter(song =>
+      {
+        const lowerSearchTerm = this.searchTerm.toLowerCase();
+        const sameId = song.id.toString() === this.searchTerm;
+        const includesName = song.name.toLowerCase().includes(lowerSearchTerm);
+        const includesSortName = song.sortName.toLowerCase().includes(lowerSearchTerm);
+        const includesArtist = song.artistName.toLowerCase().includes(lowerSearchTerm);
+        return sameId || includesName || includesSortName || includesArtist;
+      });
+    } else {
+      this.filteredSongList = [...this.songList];
+    }
   }
 
 }
