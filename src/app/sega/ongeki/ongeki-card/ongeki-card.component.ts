ce7f58a1acd7fd78631b391867b3e9216686d676
三黑 <w953934508@outlook.com>
2023-12-12 20:08:26 +0800

    [ongeki] Make card cooler

--- src/app/sega/ongeki/ongeki-card/ongeki-card.component.ts
+++ src/app/sega/ongeki/ongeki-card/ongeki-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, QueryList, Renderer2, ViewChildren} from '@angular/core';
 import {HttpParams} from '@angular/common/http';
 import {ApiService} from '../../../api.service';
 import {AuthenticationService} from '../../../auth/authentication.service';
@@ -19,15 +19,53 @@ import {ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./ongeki-card.component.css']
 })
 export class OngekiCardComponent implements OnInit {
+  protected readonly Math = Math;
 
   host = environment.assetsHost;
   enableImages = environment.enableImages;
 
   cardList: Observable<PlayerCard[]>;
-  loading: boolean;
+  loading = true;
 
   currentPage = 1;
   totalElements = 0;
+
+  isTouchDevice: boolean;
+
+  onMouseMove(event: MouseEvent, cardRotator: HTMLDivElement): void {
+    const rect = cardRotator.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = (centerY - y) / 10;
+    const rotateY = (x - centerX) / 10;
+
+    cardRotator.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateY(0) translateZ(60px)`;
+
+
+    // 计算伪元素的位置
+    const max = Math.sqrt(centerX * centerX + centerY * centerY);
+    const dx = (x - centerX) / max;
+    const dy = (y - centerY) / max;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    // 更新伪元素样式
+    const pseudoLeft = (x / rect.width) * 100 + '%';
+    const pseudoTop = (y / rect.height) * 100 + '%';
+    cardRotator.style.setProperty('--pseudo-left', pseudoLeft.toString());
+    cardRotator.style.setProperty('--pseudo-top', pseudoTop.toString());
+    cardRotator.style.setProperty('--pseudo-opacity', Math.min(1, distance).toString());
+  }
+
+  onMouseLeave(cardRotator: HTMLDivElement): void {
+    cardRotator.style.transform = 'rotateX(0) rotateY(0) rotateY(0)';
+
+    cardRotator.style.setProperty('--pseudo-translate', 'translate(-50%, -50%) translateZ(0px)');
+    cardRotator.style.setProperty('--pseudo-opacity', '0');
+  }
+
 
   constructor(
     private api: ApiService,
@@ -35,11 +73,13 @@ export class OngekiCardComponent implements OnInit {
     private auth: AuthenticationService,
     private messageService: MessageService,
     private dbService: NgxIndexedDBService,
-    public router: Router
+    public router: Router,
+    private renderer: Renderer2
   ) {
   }
+
   ngOnInit() {
-    this.loading = true;
+    this.isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
     this.route.queryParams.subscribe((data) => {
       if (data.page) {
         this.currentPage = data.page;
@@ -101,10 +141,66 @@ export class OngekiCardComponent implements OnInit {
     return new Array(n);
   }
 
-  getStarCount(item: PlayerCard){
+  getStarCount(item: PlayerCard) {
     return (item.maxLevel - (item.kaikaDate === '0000-00-00 00:00:00.0' ? 5 : 45)) / 5;
   }
 
+  calculateAtk(level: number, levelParams: number[], isChokaika: boolean): number {
+    if (levelParams === null) {
+      return null;
+    }
+    if (isChokaika) {
+      return levelParams[levelParams.length - 1];
+    }
 
+    const levels = [1, 50, 55, 60, 65, 70, 80, 90, 100];
 
+    if (level < levels[0] || level > levels[levels.length - 2]) {
+      throw new Error('Invalid level');
+    }
+
+    for (let i = 0; i < levels.length - 1; i++) {
+      if (level >= levels[i] && level < levels[i + 1]) {
+        const diff = levels[i + 1] - levels[i];
+        const ratio = (level - levels[i]) / diff;
+        const atkDiff = levelParams[i + 1] - levelParams[i];
+        return Math.floor(levelParams[i] + ratio * atkDiff);
+      }
+    }
+
+    throw new Error('Level not found in range');
+  }
+
+  convertToNumberArray(input: string): number[] {
+    if (input === null) {
+      return null;
+    }
+    return input.split(',').map(str => parseFloat(str.trim()));
+  }
+
+  getCardBackground(card: PlayerCard) {
+    const cardIdStr = card.cardId.toString().padStart(6, '0');
+    if (!card.cardInfo) {
+      return ''; // 'url(' + this.host + 'assets/ongeki/card/UI_Card_' + cardIdStr + '.jpg)';
+    }
+    const charaP = this.host + 'assets/ongeki/card-chara-p/UI_Card_Chara_' + cardIdStr + '_P.png';
+    const chara = this.host + 'assets/ongeki/card-chara-p/UI_Card_Chara_' + cardIdStr + '.png';
+    const charaUrl = 'url(' + charaP + ')';
+    let bg: string;
+    if (card.cardInfo.rarity === 'N' || card.cardInfo.rarity === 'R') {
+      if (card.cardInfo.attribute === 'Fire') {
+        bg = this.host + 'assets/ongeki/card-bg/UI_Card_BG_' + card.cardInfo.rarity + '_00.png';
+      } else if (card.cardInfo.attribute === 'Aqua') {
+        bg = this.host + 'assets/ongeki/card-bg/UI_Card_BG_' + card.cardInfo.rarity + '_01.png';
+      } else if (card.cardInfo.attribute === 'Leaf') {
+        bg = this.host + 'assets/ongeki/card-bg/UI_Card_BG_' + card.cardInfo.rarity + '_02.png';
+      }
+    }
+    if (bg != null) {
+      const bgUrl = 'url(' + bg + ')';
+      return charaUrl + ',' + bgUrl;
+    } else {
+      return charaUrl;
+    }
+  }
 }
