ffdafafba6e75ec536d3b533523a86e69f33372f
xuanxuan-0403 <1302030435@qq.com>
2024-01-22 14:37:55 +0800

    [chusan home] scss media updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import {environment} from '../../environments/environment';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, OnDestroy {
   @ViewChildren('faultLogo') faultLogos: QueryList<ElementRef>;
