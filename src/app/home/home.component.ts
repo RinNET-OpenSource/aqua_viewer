ec95772d9ca3acb15b2db68a3ea4661d4e813f72
xuanxuan-0403 <1302030435@qq.com>
2024-01-23 16:54:19 +0800

    [home] pgae layout updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import {environment} from '../../environments/environment';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, OnDestroy {
   @ViewChildren('faultLogo') faultLogos: QueryList<ElementRef>;
