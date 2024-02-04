13d10807b2ed804417533fac8a2152474805b531
Rinne <zyheng2016@gmail.com>
2024-02-04 23:22:31 +0800

    [ongeki] use gacha api provided by backend.

--- src/app/sega/ongeki/ongeki-card-gacha/ongeki-card-gacha.component.ts
+++ src/app/sega/ongeki/ongeki-card-gacha/ongeki-card-gacha.component.ts
@@ -5,9 +5,6 @@ import {ApiService} from '../../../api.service';
 import {AuthenticationService} from '../../../auth/authentication.service';
 import {MessageService} from '../../../message.service';
 import {NgxIndexedDBService} from 'ngx-indexed-db';
-import {animate, style, transition, trigger} from '@angular/animations';
-import {Bitmap, Shadow, Shape, Stage, Ticker} from '@createjs/easeljs';
-import {Ease, Timeline, Tween} from '@createjs/tweenjs';
 
 @Component({
   selector: 'app-ongeki-card-gacha',
@@ -21,50 +18,21 @@ export class OngekiCardGachaComponent implements OnInit, AfterViewInit {
 
   isStarted = false;
 
-  rarity: number[] = [];
-  rList: OngekiCard[] = [];
-  srList: OngekiCard[] = [];
-  ssrList: OngekiCard[] = [];
-
-  cardResultList: CardResult[] = [];
+  cardResultList: OngekiCard[] = [];
   currentShowedIndex = 0;
-  showCurrentCard = false;
 
   submitSuccessful = 0;
 
   constructor(
     private api: ApiService,
     private auth: AuthenticationService,
-    private messageService: MessageService,
-    private dbService: NgxIndexedDBService
   ) {
   }
 
   ngOnInit() {
-    this.rarity = Array(70).fill(1);
-    this.rarity = this.rarity.concat(Array(25).fill(2));
-    this.rarity = this.rarity.concat(Array(5).fill(3));
-    this.dbService.getAll<OngekiCard>('ongekiCard').subscribe(
-      x => {
-        x.forEach(y => {
-          switch (y.rarity) {
-            case 'R':
-              this.rList.push(y);
-              break;
-            case 'SR':
-              this.srList.push(y);
-              break;
-            case 'SSR':
-              this.ssrList.push(y);
-              break;
-          }
-        });
-      }
-    );
   }
 
   ngAfterViewInit() {
-    // this.initAnimation();
   }
 
   gacha(count: number) {
@@ -73,46 +41,15 @@ export class OngekiCardGachaComponent implements OnInit, AfterViewInit {
     this.isStarted = true;
     this.submitSuccessful = 0;
 
-    for (let i = 0; i < count; i++) {
-      const rarity = this.rarity[Math.floor(Math.random() * 99)];
-      //console.log('rarity: ' + rarity);
-      let card;
-      switch (rarity) {
-        case 1:
-          card = this.rList[Math.floor(Math.random() * this.rList.length)];
-          break;
-        case 2:
-          card = this.srList[Math.floor(Math.random() * this.srList.length)];
-          break;
-        case 3:
-          card = this.ssrList[Math.floor(Math.random() * this.ssrList.length)];
-          break;
-      }
-      //console.log('card: ' + JSON.stringify(card));
-      this.cardResultList.push({
-        show: false,
-        card
-      });
-    }
-    this.submitCardData();
-    return;
-  }
-
-  submitCardData() {
-    const aimeId = this.auth.currentAccountValue.currentCard.extId;
-    this.cardResultList.forEach(x => {
-      this.api.post('api/game/ongeki/card', {
-        aimeId,
-        cardId: x.card.id
-      }).subscribe(
-        data => {
-          this.submitSuccessful = this.submitSuccessful + 1;
-          return;
-        },
-        error => this.messageService.notice(error)
-      );
+    const requestData: GachaRequest = {
+      aimeId: this.auth.currentAccountValue.currentCard.extId.toString(),
+      count,
+    };
+    this.api.post('api/game/ongeki/gacha', requestData).subscribe(res => {
+      this.cardResultList = res;
     });
   }
+
 
   goBack() {
     this.isStarted = false;
@@ -128,7 +65,7 @@ export class OngekiCardGachaComponent implements OnInit, AfterViewInit {
 
 }
 
-interface CardResult {
-  show: boolean;
-  card: OngekiCard;
+interface GachaRequest {
+  aimeId: string;
+  count: number;
 }
