8af26de32f65587a7bcc11afa5d0884e427f7d5f
xuanxuan-0403 <1302030435@qq.com>
2024-04-26 18:14:03 +0800

    update style of not-found page

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, QueryList, Renderer2, ViewChildren} from '@angular/core';
+import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'app-not-found',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./not-found.component.css']
 })
 export class NotFoundComponent {
+  @ViewChildren('faultTurtle') faultTurtle: QueryList<ElementRef>;
+  faultTimer = null;
+  host = environment.assetsHost;
+  constructor(
+    private renderer: Renderer2,
+  ) {
+  }
 
+
+  ngOnInit(): void {
+    clearInterval(this.faultTimer);
+    this.faultTimer = setInterval(() => {
+      this.faultTurtle.forEach((img) => {
+        this.renderer.setStyle(img.nativeElement, 'transform', `translate(${Math.random() * 60 - 30}%, ${Math.random() * 60 - 30}%)`);
+        this.renderer.addClass(img.nativeElement, 'logo-img_fault');
+        const x = Math.random() * 100;
+        const y = Math.random() * 100;
+        const h = Math.random() * 50 + 50;
+        const w = Math.random() * 40 + 10;
+        this.renderer.setStyle(img.nativeElement, 'clipPath', `
+        polygon(${x}% ${y}%, ${x + w}% ${y}%, ${x + w}% ${y + h}%, ${x}% ${y + h}%)`);
+      });
+    }, 30);
+    setTimeout(() => this.faultStop(), 3000);
+  }
+
+  faultStop(): void {
+    clearInterval(this.faultTimer);
+    this.faultTurtle.forEach((img) => {
+      this.renderer.removeClass(img.nativeElement, 'logo-img_fault');
+      this.renderer.setStyle(img.nativeElement, 'transform', '');
+      this.renderer.setStyle(img.nativeElement, 'clipPath', '');
+    });
+  }
 }
+
