06ce5a704894b5383d02c0e157d2e4336bf3ef10
Sanheiii <35133371+Sanheiii@users.noreply.github.com>
2024-10-28 00:53:48 +0800

    [app] Remove unused references.

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,6 @@
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {BehaviorSubject, Subject} from 'rxjs';
+import {BehaviorSubject} from 'rxjs';
 import {environment} from '../environments/environment';
 
 @Injectable({
