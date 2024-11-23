c2dee2102be5d0c996c97ab4759d6fe954a27f06
xuanxuan-0403 <1302030435@qq.com>
2024-11-24 00:31:55 +0800

    feat(chuni-v2): add change name symbol selection

--- src/app/sega/chunithm/v2/v2-setting/v2-name-setting/v2-name-setting.dialog.ts
+++ src/app/sega/chunithm/v2/v2-setting/v2-name-setting/v2-name-setting.dialog.ts
@@ -5,14 +5,33 @@ import {V2SettingComponent} from '../v2-setting.component';
 @Component({
   selector: 'v2-name-setting-dialog',
   templateUrl: 'v2-name-setting.html',
+  styleUrls: ['v2-name-setting.scss'],
 })
 export class V2NameSettingDialog {
 
   parentComponent: any;
   @Input() public data: V2SettingComponent;
+  userName: string;
+  userNameBtnsSymbol = [
+    'Ａ', 'Ｂ', 'Ｃ', 'Ｄ', 'Ｅ', 'Ｆ', 'Ｇ', 'Ｈ', 'Ｉ', 'Ｊ', 'Ｋ', 'Ｌ', 'Ｍ', 'Ｎ', 'Ｏ', 'Ｐ', 'Ｑ', 'Ｒ', 'Ｓ', 'Ｔ', 'Ｕ', 'Ｖ', 'Ｗ', 'Ｘ', 'Ｙ', 'Ｚ',
+    '．', '・', '：', '；', '？', '！', '～', '／', '＋', '－', '×', '÷', '＝', '♂', '♀', '∀', '＃', '＆', '＊', '＠', '☆',
+    '○', '◎', '◇', '□', '△', '▽', '♪', '†', '‡', 'Σ', 'α', 'β', 'γ', 'θ', 'φ', 'ψ', 'ω', 'Д', 'ё'
+  ];
   constructor(
     public modalService: NgbModal,
   ) {
+  }
+
+  ngOnInit(): void {
+    this.userName = this.data.userName as unknown as string;
+  }
+
+
+  insertCharacter(item: string) {
+    if (item) {
+      const oldUserName = this.userName;
+      this.userName = oldUserName + item;
+    }
   }
 
 }
