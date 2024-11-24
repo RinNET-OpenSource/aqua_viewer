577efc6ddcd36fb0a2a0c72bbb7d9a1e34078eb7
HoshimiRIN <admin@sakuramoe.dev>
2024-11-24 21:33:24 +0800

    feat(app): update status bar when theme change and apply correct theme-color in PWA mode

--- src/app/theme.service.ts
+++ src/app/theme.service.ts
@@ -1,21 +1,27 @@
-import { Injectable, Renderer2 } from '@angular/core';
+import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
+  private renderer: Renderer2;
   private Theme: 'dark' | 'light' | 'auto';
   private ThemeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
-  constructor() {
+  constructor(private rendererFactory: RendererFactory2) {
     const savedTheme = localStorage.getItem('colorTheme') as 'dark' | 'light' | 'auto';
     this.Theme = savedTheme || 'auto';
-    this.applyColorTheme();
+    this.renderer = this.rendererFactory.createRenderer(null, null);
 
-    this.ThemeMediaQuery.addEventListener('change', () => {
-      if (this.Theme === 'auto') {
-        this.applyColorTheme();
-      }
+    // wait for dom
+    this.waitForDomReady(() => {
+      this.applyColorTheme();
+
+      this.ThemeMediaQuery.addEventListener('change', () => {
+        if (this.Theme === 'auto') {
+          this.applyColorTheme();
+        }
+      });
     });
   }
 
@@ -28,9 +34,37 @@ export class ThemeService {
   private applyColorTheme() {
     const isDarkMode = this.Theme === 'dark' || (this.Theme === 'auto' && this.ThemeMediaQuery.matches);
     document.documentElement.setAttribute('data-bs-theme', isDarkMode ? 'dark' : 'light');
+
+    this.setStatusBarColor(isDarkMode);
   }
 
   getTheme() {
     return this.Theme;
+  }
+
+  private setStatusBarColor(isDarkMode: boolean) {
+    // For Android
+    this.updateMetaTag('theme-color', isDarkMode ? '#2a2f33' : '#f9fafa');
+
+    // For iOS
+    this.updateMetaTag('apple-mobile-web-app-status-bar-style', isDarkMode ? 'black' : 'default');
+  }
+
+  private updateMetaTag(name: string, content: string): void {
+    let metaTag = document.querySelector(`meta[name="${name}"]`);
+    if (!metaTag) {
+      metaTag = this.renderer.createElement('meta');
+      this.renderer.setAttribute(metaTag, 'name', name);
+      this.renderer.appendChild(document.head, metaTag);
+    }
+    this.renderer.setAttribute(metaTag, 'content', content);
+  }
+
+  private waitForDomReady(callback: () => void): void {
+    if (document.readyState === 'loading') {
+      document.addEventListener('DOMContentLoaded', callback);
+    } else {
+      callback();
+    }
   }
 }
