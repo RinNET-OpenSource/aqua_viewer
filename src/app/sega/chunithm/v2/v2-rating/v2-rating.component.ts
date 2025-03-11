8596e7d1fc666df03b7301feda9ea74cf17c551b
Sanheiii <35133371+Sanheiii@users.noreply.github.com>
2025-03-12 00:50:56 +0800

    [chusan] Show b50 to verse player

--- src/app/sega/chunithm/v2/v2-rating/v2-rating.component.ts
+++ src/app/sega/chunithm/v2/v2-rating/v2-rating.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Version} from '@angular/core';
 import {MessageService} from '../../../../message.service';
 import {ApiService} from '../../../../api.service';
 import {HttpParams} from '@angular/common/http';
 import {AuthenticationService} from '../../../../auth/authentication.service';
 import {environment} from '../../../../../environments/environment';
 import { UserService } from 'src/app/user.service';
+import {lastValueFrom} from 'rxjs';
 
 @Component({
   selector: 'app-v2-rating',
@@ -16,9 +17,12 @@ export class V2RatingComponent implements OnInit {
   host = environment.assetsHost;
   enableImages = environment.enableImages;
 
+  romVersion: Version;
   topRating: RatingItem[] = [];
+  newRating: RatingItem[] = [];
   recentRating: RatingItem[] = [];
   topTotal = 0;
+  newTotal = 0;
   recentTotal = 0;
 
   constructor(
@@ -28,30 +32,54 @@ export class V2RatingComponent implements OnInit {
   ) {
   }
 
-  ngOnInit() {
+  async ngOnInit() {
     const aimeId = String(this.userService.currentUser.defaultCard.extId);
     const param = new HttpParams().set('aimeId', aimeId);
-    this.api.get('api/game/chuni/v2/rating', param).subscribe(
-      data => {
-        this.topRating = data;
-        if (this.topRating.length === 0) {
-          this.messageService.notice('No Data');
-        }
-        this.topRating.forEach(item => this.topTotal += item.rating);
-      },
-      error => this.messageService.notice(error)
-    );
-
-    this.api.get('api/game/chuni/v2/rating/recent', param).subscribe(
-      data => {
-        this.recentRating = data;
-        if (this.recentRating.length === 0) {
-          this.messageService.notice('No Data');
-        }
-        this.recentRating.forEach(item => this.recentTotal += item.rating);
-      },
-      error => this.messageService.notice(error)
-    );
+    const profile = await lastValueFrom(this.api.get('api/game/chuni/v2/profile', param));
+    if (!profile) { return; }
+    this.romVersion = new Version(profile.lastRomVersion);
+    this.messageService.notice(this.romVersion.full);
+    if (this.romVersion >= new Version('2.30.00')){
+      // b50
+      this.api.get('api/game/chuni/v2/verse-rating', param).subscribe(
+        data => {
+          this.topRating = data.old;
+          if (this.topRating.length === 0) {
+            this.messageService.notice('No Data');
+          }
+          this.newRating = data.new;
+          if (this.newRating.length === 0) {
+            this.messageService.notice('No Data');
+          }
+          this.topRating.forEach(item => this.topTotal += item.rating);
+          this.newRating.forEach(item => this.newTotal += item.rating);
+        },
+        error => this.messageService.notice(error)
+      );
+    }
+    else{
+      // b30
+      this.api.get('api/game/chuni/v2/rating', param).subscribe(
+        data => {
+          this.topRating = data;
+          if (this.topRating.length === 0) {
+            this.messageService.notice('No Data');
+          }
+          this.topRating.forEach(item => this.topTotal += item.rating);
+        },
+        error => this.messageService.notice(error)
+      );
+      this.api.get('api/game/chuni/v2/rating/recent', param).subscribe(
+        data => {
+          this.recentRating = data;
+          if (this.recentRating.length === 0) {
+            this.messageService.notice('No Data');
+          }
+          this.recentRating.forEach(item => this.recentTotal += item.rating);
+        },
+        error => this.messageService.notice(error)
+      );
+    }
   }
 
 }
