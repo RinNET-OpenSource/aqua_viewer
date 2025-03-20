87b541850af967580c45081a93159c33752982e1
Sanheiii <35133371+Sanheiii@users.noreply.github.com>
2025-03-21 01:45:58 +0800

    [chusan] Update styles for RatingPage

--- src/app/sega/chunithm/v2/v2-rating/v2-rating.component.ts
+++ src/app/sega/chunithm/v2/v2-rating/v2-rating.component.ts
@@ -6,6 +6,10 @@ import {AuthenticationService} from '../../../../auth/authentication.service';
 import {environment} from '../../../../../environments/environment';
 import { UserService } from 'src/app/user.service';
 import {lastValueFrom} from 'rxjs';
+import {ChusanMusic} from '../model/ChusanMusic';
+import {V2SongScoreRankingComponent} from '../v2-song-score-ranking/v2-song-score-ranking.component';
+import {NgbOffcanvas} from '@ng-bootstrap/ng-bootstrap';
+import {NgxIndexedDBService} from 'ngx-indexed-db';
 
 @Component({
   selector: 'app-v2-rating',
@@ -18,7 +22,12 @@ export class V2RatingComponent implements OnInit {
   enableImages = environment.enableImages;
 
   romVersion: Version;
+  playerRating: number;
+  highestRating: number;
+  calcRating: number;
   verseVersion = new Version('2.30.00');
+  loadingRating = true;
+  loadingRecent = true;
   topRating: RatingItem[] = [];
   newRating: RatingItem[] = [];
   recentRating: RatingItem[] = [];
@@ -29,7 +38,9 @@ export class V2RatingComponent implements OnInit {
   constructor(
     private api: ApiService,
     private userService: UserService,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private offcanvasService: NgbOffcanvas,
+    private dbService: NgxIndexedDBService,
   ) {
   }
 
@@ -39,49 +50,70 @@ export class V2RatingComponent implements OnInit {
     const profile = await lastValueFrom(this.api.get('api/game/chuni/v2/profile', param));
     if (!profile) { return; }
     this.romVersion = new Version(profile.lastRomVersion);
+    this.playerRating = profile.playerRating;
+    this.highestRating = profile.highestRating;
     if (this.romVersion >= new Version('2.30.00')){
       // b50
-      this.api.get('api/game/chuni/v2/verse-rating', param).subscribe(
-        data => {
-          this.topRating = data.old;
-          if (this.topRating.length === 0) {
-            this.messageService.notice('No Data');
-          }
-          this.newRating = data.new;
-          if (this.newRating.length === 0) {
-            this.messageService.notice('No Data');
-          }
-          this.topRating.forEach(item => this.topTotal += item.rating);
-          this.newRating.forEach(item => this.newTotal += item.rating);
-        },
-        error => this.messageService.notice(error)
-      );
+      const data = await lastValueFrom(this.api.get('api/game/chuni/v2/verse-rating', param));
+      if (data) {
+        this.topRating = data.old;
+        if (this.topRating.length === 0) {
+          this.messageService.notice('No Data');
+        }
+        this.newRating = data.new;
+        if (this.newRating.length === 0) {
+          this.messageService.notice('No Data');
+        }
+
+        this.topRating.forEach(item => this.topTotal += item.rating);
+        this.newRating.forEach(item => this.newTotal += item.rating);
+        this.calcRating = (this.topTotal + this.newTotal) / 50;
+      }
+      this.loadingRating = false;
+      this.loadingRecent = false;
     }
     else{
       // b30
-      this.api.get('api/game/chuni/v2/rating', param).subscribe(
-        data => {
-          this.topRating = data;
-          if (this.topRating.length === 0) {
-            this.messageService.notice('No Data');
-          }
-          this.topRating.forEach(item => this.topTotal += item.rating);
-        },
-        error => this.messageService.notice(error)
-      );
-      this.api.get('api/game/chuni/v2/rating/recent', param).subscribe(
-        data => {
-          this.recentRating = data;
-          if (this.recentRating.length === 0) {
-            this.messageService.notice('No Data');
-          }
-          this.recentRating.forEach(item => this.recentTotal += item.rating);
-        },
-        error => this.messageService.notice(error)
-      );
+      const best = await lastValueFrom(this.api.get('api/game/chuni/v2/rating', param));
+      if (best) {
+        this.topRating = best;
+        if (this.topRating.length === 0) {
+          this.messageService.notice('No Data');
+        }
+        this.topRating.forEach(item => this.topTotal += item.rating);
+      }
+      this.loadingRating = false;
+
+      const recent = await lastValueFrom(this.api.get('api/game/chuni/v2/rating/recent', param));
+      if (recent) {
+        this.recentRating = recent;
+        if (this.recentRating.length === 0) {
+          this.messageService.notice('No Data');
+        }
+        this.recentRating.forEach(item => this.recentTotal += item.rating);
+      }
+      this.loadingRecent = false;
+
+      if (best && recent){
+        this.calcRating = (this.topTotal + this.recentTotal) / 40;
+      }
     }
   }
 
+  showDetail(musicId: number) {
+    this.dbService.getByID<ChusanMusic>('chusanMusic', musicId).subscribe({
+      next: (music: ChusanMusic) => {
+        const offcanvasRef = this.offcanvasService.open(V2SongScoreRankingComponent, {
+          position: 'end',
+          scroll: false,
+        });
+        offcanvasRef.componentInstance.music = music;
+      },
+      error: (err: Error) => {
+        this.messageService.notice(err.message, 'danger');
+      }
+    });
+  }
 }
 
 export interface RatingItem {
